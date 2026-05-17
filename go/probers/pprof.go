37cada30d166178a6eeece2caaad9c35f0b7a23f
Rahul Yadav <rahulyadavsep92@gmail.com>
2026-05-17 10:33:13 +0530

    add profile block

--- go/probers/pprof.go
+++ go/probers/pprof.go
@@ -4,12 +4,22 @@ import (
 	"log"
 	"net/http"
 	_ "net/http/pprof"
+	"runtime"
 )
 
 func startPprofServer(cfg config) {
 	if !cfg.enablePprof {
 		return
 	}
+	mutexFraction := getEnvInt("PPROF_MUTEX_PROFILE_FRACTION", 5)
+	if mutexFraction > 0 {
+		runtime.SetMutexProfileFraction(mutexFraction)
+	}
+	blockRate := getEnvInt("PPROF_BLOCK_PROFILE_RATE", 1000000)
+	if blockRate > 0 {
+		runtime.SetBlockProfileRate(blockRate)
+	}
+	log.Printf("pprof profiling enabled addr=%s mutex_fraction=%d block_rate=%d", cfg.pprofAddr, mutexFraction, blockRate)
 	go func() {
 		log.Printf("pprof listening on %s", cfg.pprofAddr)
 		if err := http.ListenAndServe(cfg.pprofAddr, nil); err != nil {
